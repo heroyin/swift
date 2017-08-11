af29be1306362c5cca6fa514532f3ed7dbb078f6
hero <hero@soft2web.local>
2017-08-11 14:39:10 +0800

    release 版本编译通过

--- incredibuild_release.bat
+++ incredibuild_release.bat
@@ -1,9 +1,9 @@
 @echo off
-echo release = 1  >config.py
+echo qt = "c:\\Qt\\Qt5.6.2\\5.6\\msvc2013" >config.py
+
+echo qt5 = 1  >>config.py
+echo release = 1  >>config.py
 echo debug = 0  >>config.py
-echo HAVE_LIBXML = 1 >> config.py
-echo HAVE_SCHANNEL = 1 >> config.py
-echo HAVE_OPENSSL = 0 >> config.py
 
 setlocal ENABLEDELAYEDEXPANSION
 set word=\\
@@ -12,9 +12,13 @@ set str=%str:\=!word!%
 echo %str%
 
 echo openssl = "%str%"  >>config.py
- 
+echo HAVE_SCHANNEL = True >> config.py
+echo HAVE_OPENSSL = False >> config.py
+
+
 XGConsole /command="scons -u -j16 swiften" /openmonitor /profile="incredibuild.xml"
 
 copy %cd%\swiften\swiften.lib %cd%\swiften_release.lib 
 copy %cd%\3rdParty\Boost\Swiften_Boost.lib %cd%\3rdParty\Boost\Swiften_Boost_release.lib
 copy %cd%\3rdParty\EasyLogging\EasyLogging.lib %cd%\EasyLogging_release.lib
+
